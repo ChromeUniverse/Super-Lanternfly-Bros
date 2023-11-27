8805589ed99f9923379ffe6d572aac21e03d17b0
Lucca Duarte Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2023-11-27 16:05:55 -0500

    Updated entrypoint code and readme

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
 from cmu_graphics import *
+
 from classes.Player import Player
 from classes.Block import Block
 from classes.PowerUpBlock import PowerUpBlock
 from classes.BB import BB
 from classes.Camera import Camera
+from classes.Lanternfly import Lanternfly
+from classes.Coin import Coin
+from classes.Lanternfly2 import Lanternfly2
+from classes.Goal import Goal
+
 import csv
+import time
+
+
+def drawUI(app):
+    font = "monospace"
+
+    # Player name & healthbar
+    hpBarWidth = 350
+    drawLabel(app.player.name, 100, 50, size=36, align="left", font=font, bold=True)
+    drawLabel("HP", 100, 100, size=36, align="left", font=font)
+    drawRect(160, 82, hpBarWidth, 35, fill="red")
+    if app.player.HP > 0:
+        drawRect(
+            160,
+            82,
+            (hpBarWidth * app.player.HP / app.player.maxHP),
+            35,
+            fill="lightGreen",
+        )
+
+    # Score
+    drawLabel("Score", 700, 50, size=36, align="left", font=font, opacity=50)
+    drawLabel(app.score, 700, 100, size=36, align="left", font=font, bold=True)
+
+    # Level indicator
+    drawLabel("Level", 1000, 50, size=36, align="left", font=font, opacity=50)
+    drawLabel(0, 1000, 100, size=36, align="left", font=font, bold=True)
+
+    # Time left
+    drawLabel("Time", 1300, 50, size=36, align="left", font=font, opacity=50)
+    elapsed = time.time() if app.endTime == None else app.endTime
+    drawLabel(
+        (elapsed - app.startTime) * 1000 // 1,
+        1300,
+        100,
+        size=36,
+        align="left",
+        font=font,
+        bold=True,
+    )
 
 
 def loadLevel():
@@ -13,13 +59,24 @@ def loadLevel():
     with open(levelFileName, newline="") as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
         for row in reader:
-            levelData.append([int(block) for block in row])
+            levelData.append(row)
     return levelData
 
 
 def onAppStart(app):
+    app.score = 0
+    app.gameOver = False
+
+    # initialize timing
     app.counter = 0
+    app.startTime = time.time()
+    app.endTime = None
+
+    # entities
     app.blocks = []
+    app.enemies = []
+    app.coins = []
+    app.goal = None
 
     # initialize camera
     app.camera = Camera(app, 700, 0, 300, app.height)
@@ -30,19 +87,34 @@ def onAppStart(app):
     for i in range(len(level)):
         row = level[i]
         for j in range(len(row)):
-            if level[i][j] == 1:
-                app.blocks.append(Block(app, j * 100, i * 100))
-            elif level[i][j] == 2:
-                app.blocks.append(PowerUpBlock(app, j * 100, i * 100))
+            x, y = j * 100, i * 100
+            cell = level[i][j]
+            if cell == "1":
+                app.blocks.append(Block(app, x, y))
+            elif cell == "2":
+                app.blocks.append(PowerUpBlock(app, x, y))
+            elif cell == "3":
+                app.enemies.append(Lanternfly(app, x, y))
+            elif cell == "4":
+                app.coins.append(Coin(app, x, y))
+            elif cell == "5":
+                app.blocks.append(PowerUpBlock(app, x, y, type=1))
+            elif cell == "6":
+                app.enemies.append(Lanternfly2(app, x, y))
+            elif cell == "7":
+                app.goal = Goal(app, x, y)
 
     # initialize player
     app.player = Player(app)
 
     # game framerate
-    # app.stepsPerSecond = 5
+    # app.stepsPerSecond = 10
 
 
 def onKeyHold(app, keys):
+    if app.gameOver:
+        return
+
     if "right" in keys:
         app.player.moveRight()
     if "left" in keys:
@@ -57,16 +129,29 @@ def onKeyRelease(app, key):
 
 
 def redrawAll(app):
-    app.player.draw()
+    for enemy in app.enemies:
+        enemy.draw()
+
     for block in app.blocks:
         block.draw()
+
+    for coin in app.coins:
+        coin.draw()
+
+    app.goal.draw()
     app.camera.draw()
+
+    app.player.draw()
+    drawUI(app)
 
 
 def takeStep(app):
     app.counter += 1
     app.player.step()
     app.camera.step()
+
+    for enemy in app.enemies:
+        enemy.step()
 
 
 def onStep(app):
--- readme.md
+++ readme.md
@@ -4,21 +4,28 @@ Term Project submission for 15-112 Fall 2023.
 
 Author: Lucca Duarte Rodrigues (**luccar**)
 
-# Roadmap
+# Preliminary roadmap
 
 - Next few days: 
-    - basic level designer
-    - at least one level
-    - side-scrolling
-    - player movement
-    - OOP scaffolding
+    - [x] basic level designer
+    - [x] at least one level
+    - [x] side-scrolling
+    - [x] player movement
+    - [x] OOP scaffolding
 - End of week/TP1
-    - designs/sprites for characters and levels
-    - collision detection
-    - refined OOP implementation
-    - basic UI
-- TP2: Screens (title screen, gameplay screen, menu, etc.), refined UI, power-ups, most enemies implemented
-- TP3: Levels finalized, boss battle, and final gameplay tweaks
+    - [ ] designs/sprites for characters and levels
+    - [x] collision detection
+    - [x] refined OOP implementation
+    - [x] basic UI
+- TP2: 
+    - [ ] Screens (title screen, gameplay screen, menu, etc.)
+    - [ ] refined UI
+    - [ ] Power-ups
+    - [ ] Most enemies implemented
+- TP3
+    - [ ] Levels finalized
+    - [ ] Boss battle
+    - [ ] Final gameplay tweaks
 
 ## Local setup
 
